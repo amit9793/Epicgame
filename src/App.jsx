25ab7c128f8407aa0ccbe8b659521e0b1e9c01a5
Sharoof Khan <Sharoofskhan10@gmail.com>
2022-01-23 10:34:45 +0530

    Home Page Complete

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Cart } from './components/carousels/Cart';
+import { Game } from './components/carousels/Game';
+import {Homepage} from './components/Homepage'
+
+export const App = () => {
+    return (
+         < div >
+            <Homepage />
+            {/* <Cart/> */}
+            {/* <Game/> */}
+           
+        
+    
+      
+  </div >
+    )
+};
