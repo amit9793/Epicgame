784d7bec09d38b47b20dbe673c50fd609ef83aaa
Sharoof Khan <Sharoofskhan10@gmail.com>
2022-01-23 11:59:50 +0530

    GameDetails Page completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import { Cart } from './components/carousels/Cart';
 import { Game } from './components/carousels/Game';
+import { Details } from './components/gameDetails/Details';
+import { TennisManager } from './components/gameDetails/TennisManager';
 import {Homepage} from './components/Homepage'
 
 export const App = () => {
     return (
          < div >
-            <Homepage />
+            {/* <Homepage /> */}
             {/* <Cart/> */}
             {/* <Game/> */}
-           
+           {/* <TennisManager/> */}
+           <Details/>
         
     
       
